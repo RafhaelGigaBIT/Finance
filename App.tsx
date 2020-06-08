fc4dc3ad3df4b8ba306d3ab151b8b0f6baf9531f
Rafhael Gigante <rafhael.gigante@acad.ifma.edu.br>
2020-06-08 01:01:33 -0300

    Com o grid de buttons do reports

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
-import { Poppins_200ExtraLight, useFonts } from '@expo-google-fonts/poppins'
+import {Poppins_300Light, Poppins_400Regular, Poppins_200ExtraLight, Poppins_600SemiBold ,useFonts } from '@expo-google-fonts/poppins'
 import {AppLoading} from 'expo'
 
 import Home from './src/pages/Home'
 
 export default function App() {
   const [fontActivated] = useFonts(
-    {Poppins_200ExtraLight
+    {Poppins_400Regular,
+      Poppins_300Light,
+      Poppins_200ExtraLight,
+      Poppins_600SemiBold
   })
 
   if (!fontActivated){
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {View, Text, StyleSheet, ImageBackground, Image} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity} from 'react-native'
 
 import { Feather as Icon } from '@expo/vector-icons'
 
@@ -14,7 +14,31 @@ const Home= () =>{
                 <Icon name="search" size={25} color='#fff' style={{top: -70, left: '80%'}} />
                 <Icon name="menu" size={25} color='#fff' style={{top: -97, left: '95%'}}/>        
             </View>
+            <View style={styles.balaceAcc}>
+                <Text style={{fontFamily: 'Poppins_300Light', fontSize:16 ,top: -45, left: 10, color: '#fff'}}>Your Balance</Text>
+                <Text style={{fontFamily: 'Poppins_200ExtraLight', fontSize:30 ,top: -45, left: 10, color: '#fff'}} >R$</Text>
+                <Text style={{fontFamily: 'Poppins_400Regular', fontSize:30 ,top: -100, left: 55, color: '#fff'}} >24,165.00</Text>
+            </View>
             <View style={styles.middle}>
+                <Text style={{fontFamily:'Poppins_600SemiBold', fontSize:20, color:'#992525', left:30, position:'absolute', top:25}}>Reports</Text>
+                <View style={styles.reports}>
+                    <TouchableOpacity onPress={() => {}} style={styles.buttonReports} >
+                        <Text style={{fontFamily: 'Poppins_300Light', fontSize:16, color: '#992525'}}>Day</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {}} style={styles.buttonReports} >
+                            <Text style={{fontFamily: 'Poppins_300Light', fontSize:16, color: '#992525'}}>Week</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {}} style={styles.buttonReportsSelected} >
+                            <Text style={{fontFamily: 'Poppins_300Light', fontSize:16, color: '#992525'}}>Month</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {}} style={styles.buttonReports} >
+                            <Text style={{fontFamily: 'Poppins_300Light', fontSize:16, color: '#992525'}}>Year</Text>
+                    </TouchableOpacity>
+
+                </View>
             </View>
         </ImageBackground>
     )
@@ -46,13 +70,20 @@ const styles = StyleSheet.create({
     name: {
         top: -40,
         left: 55,
-        fontFamily: 'Poppins_200ExtraLight',
+        fontFamily: 'Poppins_300Light',
         fontStyle: 'normal',
         fontSize: 18,
         lineHeight: 28,
         letterSpacing: 0.01,
 
         color: '#fff',
+    },
+
+    balaceAcc: {
+        position: 'absolute',
+        width: windowWidth - 220,
+        top: windowHeight-550,
+        left: windowWidth -395
     },
 
     middle: {
@@ -64,6 +95,32 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         borderTopLeftRadius: 55,
         borderTopRightRadius: 55,
+    },
+    reports: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: "absolute",
+        left: 15,
+        height: 50,
+        top: 60,
+    },
+
+    buttonReports: {
+        width: 100,
+        height: 40,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+    },
+    buttonReportsSelected: {
+        justifyContent: 'center',
+        width: 100,
+        height: 40,
+        paddingHorizontal: 20,
+        borderRadius: 23,
+        backgroundColor: '#C4C4C4'
+
     }
 })
 
